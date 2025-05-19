ce43a0da82011ce1408f4787c969a579680aad9f
Rahul Sharma <shubhamrushc@gmail.com>
2025-05-19 15:13:39 +0530

    Stop tracking backend folder

--- backend/models/Admin.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const AdminSchema = new mongoose.Schema({
-  email: String,
-  password: String, // hashed
-});
-
-module.exports = mongoose.model('Admin', AdminSchema);
